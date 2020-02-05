25d6b030593d347d1be79c8ec353be0cbc4f1353
lpofredc <frederic.cloitre@lpo.fr>
2020-02-05 11:28:36 +0100

    dockerization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,6 +24,7 @@ if [ ! -f /config/config.py ]; then
     sed -i "s/dbPassword/${DBPWD}/g" /config/config.py
     sed -i "s/dbPort/${DBPORT}/g" /config/config.py
     sed -i "s/secretKey/${SECRETKEY}/g" /config/config.py
+    sed -i "s/2154/${SRID}/g" /config/config.py
 else
     echo "config file already exists" 
 fi
